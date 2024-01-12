db1adae3ece51f52e2ecc2288927fa2bac14171b
Hayden Chan <hchan@lcs.on.ca>
2024-01-12 08:40:50 -0500

    Initial Commit

---
+++ Home/HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeApp.swift
+//  Home
+//
+//  Created by Hayden Chan on 2024-01-12.
+//
+
+import SwiftUI
+
+@main
+struct HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
